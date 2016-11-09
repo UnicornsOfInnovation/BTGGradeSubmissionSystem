ad13eb19e146df264ae7fbfc7193244b9aa0a3d8
Christian Dave Baclayon <davebaclayon@gmail.com>
2016-11-09 13:43:09 +0800

    Reviewed Coding Standards for Course

--- BTGGradeSubmissionSystem/src/btg/meta/StrandModelMeta.java
+++ BTGGradeSubmissionSystem/src/btg/meta/StrandModelMeta.java
@@ -1,6 +1,6 @@
 package btg.meta;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2016-11-07 18:45:15")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2016-11-09 12:54:02")
 /** */
 public final class StrandModelMeta extends org.slim3.datastore.ModelMeta<btg.model.StrandModel> {
 
