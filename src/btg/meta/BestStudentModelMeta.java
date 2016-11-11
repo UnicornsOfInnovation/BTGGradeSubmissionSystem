f4ae7dff0ab18d05a92457af4847100f7a8420cc
testfilee15 <testfilee15@gmail.com>
2016-11-11 17:07:46 +0800

    start log-in

--- src/btg/meta/BestStudentModelMeta.java
+++ src/btg/meta/BestStudentModelMeta.java
@@ -1,6 +1,6 @@
 package btg.meta;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2016-11-11 15:33:57")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2016-11-11 16:41:18")
 /** */
 public final class BestStudentModelMeta extends org.slim3.datastore.ModelMeta<btg.model.BestStudentModel> {
 
