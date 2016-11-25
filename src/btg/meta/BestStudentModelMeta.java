224b10ea5e3dd2b86cc5f1149f40dd338ef48892
Christian Dave Baclayon <davebaclayon@gmail.com>
2016-11-25 15:50:37 +0800

    Change Password for student is done

--- src/btg/meta/BestStudentModelMeta.java
+++ src/btg/meta/BestStudentModelMeta.java
@@ -1,6 +1,6 @@
 package btg.meta;
 
-//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2016-11-25 11:12:42")
+//@javax.annotation.Generated(value = { "slim3-gen", "@VERSION@" }, date = "2016-11-25 13:39:52")
 /** */
 public final class BestStudentModelMeta extends org.slim3.datastore.ModelMeta<btg.model.BestStudentModel> {
 
--- war/js/student.js
+++ war/js/student.js
@@ -32,6 +32,73 @@ angular.module('loginApp').controller('studentController', function($scope, $htt
 	$scope.newCourseList=[];
 	$scope.newCourseCodeList=[];
 	
+	$scope.pass = {
+		oldPassword: "",
+		newPassword: "",
+		confirmNewPassword: ""
+	};
+	
+	clearPasswordFields = function(){
+		$scope.pass.oldPassword="";
+		$scope.pass.newPassword="";
+		$scope.pass.confirmNewPassword="";
+	}
+	
+	$scope.changePassword = function(){
+		console.log("student.changePassword " + "START");
+		
+		
+		console.log("account id: " + $scope.studentIdFromLogin);
+		console.log("old password: " + $scope.pass.oldPassword);
+		console.log("new password: " + $scope.pass.newPassword);
+		console.log("confirm new password: " + $scope.pass.confirmNewPassword);
+		
+		if($scope.pass.oldPassword != $scope.studentAccount.password){
+			alert("Access denied! Wrong password!");
+			clearPasswordFields();
+	    }else if ($scope.pass.newPassword != $scope.pass.confirmNewPassword){
+			alert("Your new password and confirm new password didn't match");
+			clearPasswordFields();
+		} else if ($scope.pass.newPassword == $scope.pass.oldPassword){
+			alert("Your old and new passwords match! Try a new combination.");
+			clearPasswordFields();
+		} else {
+			var changePassObject = {
+			   id: $scope.studentIdFromLogin,
+			   newPassword: $scope.pass.newPassword,
+			   action: "ChangePassword"
+			};
+			
+			$http.post("/account", $httpParamSerializer(changePassObject),
+					{// configuring the request not a JSON type.
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+					}
+			)
+			
+			.then(function(response) {
+				console.log("Error? " + response.data.errorList.length);
+				if (response.data.errorList.length == 0) {
+					//There were no errors.
+					alert("Changing password was successful.");
+					// initializing the contents of the ingredient screen.
+					$('#changePassword').modal('hide');
+					$('.modal-backdrop').hide();
+				} else {
+					// display the error messages.
+					var errorMessage = "";
+					alert("Changing password was NOT successful.");
+					for (var i = 0; i < response.data.errorList.length; i++) {
+						errorMessage += response.data.errorList[i];
+					}
+					alert(errorMessage);
+				}
+			}, function() {
+				alert("An error has occured");
+			})
+		}
+		console.log("student.changePassword " + "END");
+	}
+	
 	
 	
 	$scope.getStudentAccount = function() {
